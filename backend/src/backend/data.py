eed4b9e90a2332c20c6489f7f7e9fdecc90072b6
Kristiyan Yochev <kris.yochev@gmail.com>
2023-02-11 17:25:13 +0000

    Added population code

--- backend/src/backend/data.py
+++ backend/src/backend/data.py
@@ -1,12 +1,68 @@
-from fetch_all_data import fetch_all_buildings, fetch_building_data
+from fetch_all_data import fetch_all_buildings, fetch_building_data, StatisticType
 from database import DatabaseConnection
 from sqlite3 import Connection
+import csv
+import datetime
+
+from typing import Iterable, Any
 
 
 def populate_database(database: str):
     """Populates the database with data from all the buildings"""
     with DatabaseConnection(database) as db_conn:
         create_db_schema(db_conn)
+
+        buildings = fetch_all_buildings()
+        for building in buildings:
+            building_min_csv = fetch_building_data(building, StatisticType.MINIMUM)
+            parsed = parse_csv(building_min_csv)
+            formatted = format_for_inserton(parsed)
+            with db_conn:
+                db_conn.executemany("""
+                    INSERT OR IGNORE INTO samples(building_code, timestamp) VALUES (:building, :timestamp);
+                """, formatted)
+                db_conn.executemany("""
+                    UPDATE samples SET min = :sample WHERE building_code = :building AND timestamp = :timestamp;
+                """, formatted)
+            
+            building_avg_csv = fetch_building_data(building, StatisticType.AVERAGE)
+            parsed = parse_csv(building_avg_csv)
+            formatted = format_for_inserton(parsed)
+            with db_conn:
+                db_conn.executemany("""
+                    INSERT OR IGNORE INTO samples(building_code, timestamp) VALUES (:building, :timestamp);
+                """, formatted)
+                db_conn.executemany("""
+                    UPDATE samples SET average = :sample WHERE building_code = :building AND timestamp = :timestamp;
+                """, formatted)
+
+            building_max_csv = fetch_building_data(building, StatisticType.MAXIMUM)
+            parsed = parse_csv(building_max_csv)
+            formatted = format_for_inserton(parsed)
+            with db_conn:
+                db_conn.executemany("""
+                    INSERT OR IGNORE INTO samples(building_code, timestamp) VALUES (:building, :timestamp);
+                """, formatted)
+                db_conn.executemany("""
+                    UPDATE samples SET max = :sample WHERE building_code = :building AND timestamp = :timestamp;
+                """, formatted)
+
+
+def format_for_inserton(parsed: Iterable[tuple[str]]) -> list[dict[str, Any]]:
+    return [
+        {"building": bdg, "timestamp": to_unix_timestamp(datestring), "sample": float(value)}
+        for bdg, datestring, value in parsed
+    ]
+
+
+def parse_csv(source: str) -> Iterable[tuple[str]]:
+    lines = source.splitlines()
+    return csv.reader(lines)
+
+
+def to_unix_timestamp(datestring: str) -> int:
+    date_format = datetime.datetime.strptime(datestring, "%Y-%m-%d %H:%M:%S")
+    return datetime.datetime.timestamp(date_format)
 
 
 def create_db_schema(connection: Connection):
