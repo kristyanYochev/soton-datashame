1f69055b13073c8f99600ee094f62b96734496ae
Kristiyan Yochev <kris.yochev@gmail.com>
2023-02-11 14:45:10 +0000

    Refactor docs

--- backend/src/backend/fetchAllData.py
+++ backend/src/backend/fetchAllData.py
@@ -2,10 +2,8 @@ import requests
 import re
 
 
-'''Returns all building codes from the energy-time-series-map.ttl file'''
-
-
 def fetchAllBuildings():
+    '''Returns all building codes from the energy-time-series-map.ttl file'''
     url = 'https://data.southampton.ac.uk/dumps/energy-time-series-map/2019-02-04/energy-time-series-map.ttl'
     response = requests.get(url)
     # Pattern match on "" that are not within a <>, i.e. not URIs, that begin with elec
@@ -14,13 +12,12 @@ def fetchAllBuildings():
     return buildings
 
 
-'''Returns all energy data for a given building
-    building: building code
-    resolution: 3600 for hourly data, 86400 for daily data
-'''
-
-
 def fetchBuildingData(building: str, type: str = "average", resolution=3600) -> str:
+    '''
+    Returns all energy data for a given building
+    :param building: building code
+    :param resolution: 3600 for hourly data, 86400 for daily data
+    '''
     url = 'https://data.southampton.ac.uk/time-series?action=fetch&series=' + \
         building + '&format=csv&type=' + type + '&resolution=' + \
         str(resolution) + '&startTime=0'
@@ -28,10 +25,8 @@ def fetchBuildingData(building: str, type: str = "average", resolution=3600) -> 
     return buildingMin.text
 
 
-'''Dumps data to a csv file'''
-
-
-def writeDataToFile(data: str, filename: str) -> None:
+def writeDataToFile(data: str, filename: str):
+    '''Dumps data to a csv file'''
     with open(filename, 'w') as f:
         f.write(data)
 
