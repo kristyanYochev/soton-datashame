a5b8475487e3581948326011197c695a4cb5824a
Isaac Day <16.iaday@gmail.com>
2023-02-11 14:08:09 +0000

    Saving so that I don't have another heart attack

---
+++ backend/src/backend/fetchAllData.py
@@ -0,0 +1,51 @@
+import requests
+import re
+import csv
+
+'''Returns all building codes from the energy-time-series-map.ttl file'''
+
+
+def fetchAllBuildings():
+    url = 'https://data.southampton.ac.uk/dumps/energy-time-series-map/2019-02-04/energy-time-series-map.ttl'
+    response = requests.get(url)
+    # Pattern match on "" that are not within a <>, i.e. not URIs, that begin with elec
+    buildings = re.findall(r'(?<!<)"(elec[^"]*)', response.text)
+
+    return buildings
+
+
+'''Returns all energy data for a given building
+    building: building code
+    resolution: 3600 for hourly data, 86400 for daily data
+'''
+
+
+def fetchBuildingData(building: str, type: str = "average", resolution=3600) -> str:
+    url = 'https://data.southampton.ac.uk/time-series?action=fetch&series=' + \
+        building + '&format=csv&type=' + type + '&resolution=' + \
+        str(resolution) + '&startTime=0'
+    buildingMin = requests.get(url)
+    return buildingMin.text
+
+
+'''Dumps data to a csv file'''
+
+
+def writeDataToFile(data: str, filename: str) -> None:
+    with open(filename, 'w') as f:
+        f.write(data)
+
+
+def dumpAllAvgData():
+    for building in fetchAllBuildings():
+        writeDataToFile(fetchBuildingData(building),
+                        building.replace('/', '-') + "_avg.csv")
+        print("Fetched " + building)
+
+
+def main():
+    dumpAllAvgData()
+
+
+if __name__ == "__main__":
+    main()
