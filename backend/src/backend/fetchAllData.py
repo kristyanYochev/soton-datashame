b8b43a450d4e61cf38cae6721fcf510a83fb279b
Kristiyan Yochev <kris.yochev@gmail.com>
2023-02-11 15:25:44 +0000

    Documented fetch building data better

--- backend/src/backend/fetchAllData.py
+++ backend/src/backend/fetchAllData.py
@@ -1,6 +1,8 @@
 import requests
 import re
+import urllib.parse
 from enum import Enum
+
 
 class DataResolution(Enum):
     HOURLY = 3600
@@ -13,9 +15,9 @@ class StatisticType(Enum):
     AVERAGE = "average"
 
 
-def fetchAllBuildings():
-    '''Returns all building codes from the energy-time-series-map.ttl file'''
-    url = 'https://data.southampton.ac.uk/dumps/energy-time-series-map/2019-02-04/energy-time-series-map.ttl'
+def fetchAllBuildings() -> list[str]:
+    """Returns all building codes from the energy-time-series-map.ttl file"""
+    url = "https://data.southampton.ac.uk/dumps/energy-time-series-map/2019-02-04/energy-time-series-map.ttl"
     response = requests.get(url)
     # Pattern match on "" that are not within a <>, i.e. not URIs, that begin with elec
     buildings = re.findall(r'(?<!<)"(elec[^"]*)', response.text)
@@ -24,28 +26,34 @@ def fetchAllBuildings():
 
 
 def fetchBuildingData(building: str, type: StatisticType = StatisticType.AVERAGE, resolution: DataResolution = DataResolution.HOURLY) -> str:
-    '''
-    Returns all energy data for a given building
-    :param building: building code
-    :param resolution: The sample frequency of the data
-    '''
-    url = 'https://data.southampton.ac.uk/time-series?action=fetch&series=' + \
-        building + '&format=csv&type=' + type + '&resolution=' + \
-        str(resolution) + '&startTime=0'
-    buildingMin = requests.get(url)
-    return buildingMin.text
+    """
+    Returns
+    -------
+    CSV-formatted string with the `building`'s energy consumption data
+    """
+    parameters = urllib.parse.urlencode({
+        "action": "fetch",
+        "series": building,
+        "format": "csv",
+        "type": type,
+        "resolution": resolution,
+        "startTime": 0
+    })
+    url = f"https://data.southampton.ac.uk/time-series?{parameters}"
+    energyCsv = requests.get(url)
+    return energyCsv.text
 
 
 def writeDataToFile(data: str, filename: str):
-    '''Dumps data to a csv file'''
+    """Dumps data to a csv file"""
     with open(filename, 'w') as f:
         f.write(data)
 
 
 def dumpAllData(type: StatisticType = StatisticType.AVERAGE, resolution: DataResolution = DataResolution.HOURLY):
-    '''
+    """
     Dumps all data
-    '''
+    """
     for building in fetchAllBuildings():
         writeDataToFile(fetchBuildingData(building, type, resolution),
                         "./soton-datashame/backend/src/backend/data/" + building.replace('/', '-') + "-" + type + ".csv")
