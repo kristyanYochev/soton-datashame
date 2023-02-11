29edf24d417895f40538381be09005c5d4deef1c
Kristiyan Yochev <kris.yochev@gmail.com>
2023-02-11 22:21:56 +0000

    V1 data fetching works

--- backend/src/backend/main.py
+++ backend/src/backend/main.py
@@ -1,6 +1,6 @@
 from flask import Flask, request
 from database import DatabaseConnection
-from pypika import Query
+from pypika import Query, Table, CustomFunction
 import json
 
 app = Flask(__name__)
@@ -15,9 +15,12 @@ def list_buildings():
         return json.dumps(buildings)
 
 
-@app.route("/buildings/<bdg_code>")
+@app.route("/buildings/<path:bdg_code>")
 def query_data(bdg_code: str):
-    q = Query.from_("samples")
+    strftime = CustomFunction("strftime", ["format", "timestamp"])
+    sql_datetime = CustomFunction("datetime", ["value", "format"])
+    samples = Table("samples")
+    q: Query = Query.from_(samples).where(samples.building_code == bdg_code)
     match request.args["stat_type"]:
         case "min":
             q = q.select("min")
@@ -26,8 +29,30 @@ def query_data(bdg_code: str):
         case "average":
             q = q.select("average")
         case _:
-            return "WTF?"
-    ...
+            return "Invalid stat_type", 400
+
+    if "year" in request.args:
+        q = q.where(strftime("%Y", sql_datetime(samples.timestamp, "unixepoch")) == request.args["year"])
+
+    if "month" in request.args:
+        q = q.where(strftime("%m", sql_datetime(samples.timestamp, "unixepoch")) == request.args["month"])
+
+    if "day" in request.args:
+        q = q.where(strftime("%d", sql_datetime(samples.timestamp, "unixepoch")) == request.args["day"])
+    
+    q = q.select("timestamp")
+
+    with DatabaseConnection("test.db") as db:
+        db.row_factory = lambda _, row: {"value": row[0], "timestamp": row[1]}
+        cur = db.cursor()
+
+        print(str(q))
+
+        cur.execute(str(q))
+
+        data = list(cur)
+        return json.dumps(data)
+
 
 if __name__ == "__main__":
     app.run(port=8080)
