4b4903b0c6f9b6429ab33deb72b28f537c2ee29c
Kristiyan Yochev <kris.yochev@gmail.com>
2023-02-11 15:41:22 +0000

    Added function for populating the database

---
+++ backend/src/backend/data.py
@@ -0,0 +1,5 @@
+from fetch_all_data import fetch_all_buildings, fetch_building_data
+
+def populate_database(database: str):
+    """Populates the database with data from all the buildings"""
+    pass
