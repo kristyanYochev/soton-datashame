888d98452ccdd1a7453fa626315a5a29ea402f48
Kristiyan Yochev <kris.yochev@gmail.com>
2023-02-12 00:39:57 +0000

    Cleared all of App

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -2,36 +2,11 @@ import Home from './pages/Home';
 import Charts from './pages/Charts';
 import React, { useEffect, useState } from 'react';
 import { Pages } from './utils/enums';
+import { createBrowserRouter } from 'react-router-dom';
 
-function pageToLoad(page: Pages, setPage:any) {
-  switch(page) {
-    case Pages.Home:
-      return <Home setPage={setPage}/>
-    case Pages.Charts:
-      return <Charts setPage={setPage}/>
-    default:
-      return;
-  }
-}
-
-const App = () => {
-  const [page, setPage] = useState(Pages.Home);
-
-  useEffect(() => {
-    const pageObj = localStorage.getItem('page');
-
-    const page = pageObj == null ? null: JSON.parse(pageObj); 
-    
-    setPage(page ? page : Pages.Home);
-
-  }, [])
-
-  useEffect(()=> {
-    localStorage.setItem('page', JSON.stringify(page))
-  }, [page])
-
-  return (<div className='app'>{pageToLoad(page, setPage)}</div>)
-}
+const App = () => (
+  <></>
+)
 
 export default App;
 
