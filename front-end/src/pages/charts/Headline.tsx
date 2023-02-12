c8136204be6a5f5db524c756aff97216f9575cb2
Kristiyan Yochev <kris.yochev@gmail.com>
2023-02-12 03:32:41 +0000

    'nuff design

--- front-end/src/pages/charts/Headline.tsx
+++ front-end/src/pages/charts/Headline.tsx
@@ -29,51 +29,53 @@ const Headline: React.FC = () => {
         init().catch(console.error);
     }, []);
 
+    const mostConsumingBuildingNumber =
+        headline?.most_consuming_building.building_code.split('/')[1].slice(1);
+
     return (
         <>
             {!headline && <h1>Loading...</h1>}
             {headline && (
                 <div className="cols-2-md">
                     <div className="card">
-                        <h1 className="title">Energy Consumption 2020 (KWH)</h1>
-                        <div className="content">
-                            {headline.power_consumption_2020.toFixed(0)} kWh (
-                            {(
-                                ((headline.power_consumption_2020 -
-                                    headline.power_consumption_2019) *
-                                    100) /
-                                headline.power_consumption_2019
-                            ).toFixed(2)}
-                            % )
-                        </div>
-                    </div>
-                    <div className="card">
-                        <h1 className="title">Energy Consumption 2019 (KWH)</h1>
-                        <div className="content">
-                            {headline.power_consumption_2019}KWH
-                        </div>
-                    </div>
-                    <div className="card">
                         <h1 className="title">
-                            Biggest Energy Consumer 2020 (KWH)
+                            <span className="emphasis">
+                                {headline.power_consumption_2020.toFixed(0)}
+                            </span>{' '}
+                            kWh
                         </h1>
-                        <h2 className="subtitle">
-                            Building{' '}
-                            {headline.most_consuming_building.building_code
-                                .split('/')[1]
-                                .slice(1)}
+
+                        <h2 className="sub-title">
+                            Total Energy Consumption in 2020
                         </h2>
-                        <div className="content">
-                            {headline.most_consuming_building.consumption}KWH
-                            (59%)
-                        </div>
                     </div>
                     <div className="card">
                         <h1 className="title">
-                            Biggest Energy Consumer 2021 (KWH)
+                            <span className="emphasis">
+                                {headline.power_consumption_2019.toFixed(0)}
+                            </span>{' '}
+                            kWh
                         </h1>
-                        <h2 className="subtitle">Building 59</h2>
-                        <div className="content">2500K5WH (+15%)</div>
+                        <h2 className="sub-title">
+                            Total Energy Consumption in 2019
+                        </h2>
+                    </div>
+                    <div className="card">
+                        <h1 className="title">
+                            <span className="emphasis">
+                                Building {mostConsumingBuildingNumber}
+                            </span>{' '}
+                            consumed{' '}
+                            <span className="emphasis">
+                                {headline.most_consuming_building.consumption.toFixed(
+                                    0
+                                )}
+                            </span>{' '}
+                            kWh in <span className="emphasis">2020</span>
+                        </h1>
+                        <h2 className="sub-title">
+                            Most Energy-Consuming Building in 2020
+                        </h2>
                     </div>
                 </div>
             )}
