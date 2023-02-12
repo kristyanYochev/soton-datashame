43d40a1eea8560f8ba38abbe7d290726f9dc3801
Kristiyan Yochev <kris.yochev@gmail.com>
2023-02-12 00:53:48 +0000

    Remove unnecessary imports

--- front-end/src/index.tsx
+++ front-end/src/index.tsx
@@ -1,10 +1,8 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import './styles/root.css';
 import './styles/reset.css';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from "react-router-dom";
 import App from './App';
 
 
